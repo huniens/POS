be6982f7a80335e8e6a62c8e7e22369ec8b65b62
huniens <huniens06@gmail.com>
2025-03-12 01:49:10 +0700

    JOBSHEET 4 - Practicum-2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,26 +1,26 @@
-    <?php
+<?php
 
-    namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
-    use App\Models\UserModel;
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Hash;
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
-    class UserController extends Controller
+class UserController extends Controller
+{
+    public function index()
     {
-        public function index()
-        {
-            $user = UserModel::firstOrNew(
-                [
-                    'username' => 'manager33',
-                    'nama' => 'Manager Tiga Tiga',
-                    'password' => Hash::make('12345'),
-                    'level_id' => 2
-                ],
-            );
-            $user->save();
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user->save();
 
-            return view('user', ['data' => $user]);
-        }
+        return view('user', ['data' => $user]);
     }
+}
 
