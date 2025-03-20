95ab8e24946c076a8b30f9576b9e78b6b0a127be
huniens <huniens06@gmail.com>
2025-03-20 11:31:29 +0700

    JOBSHEET 5 - Practicum 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,18 +22,27 @@ class UserController extends Controller
 
         $activeMenu = 'user'; // set menu yang sedang aktif
 
+        $level = LevelModel::all(); // ambil data level untuk filter level
+
         return view('user.index', [
             'breadcrumb' => $breadcrumb, 
             'page' => $page, 
+            'level' => $level,
             'activeMenu' => $activeMenu
         ]);
     }
 
+    // Ambil data user dalam bentuk json untuk datatable
     public function list(Request $request)
-{
+    {
     $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
         ->with('level');
 
+    // Filter data user berdasarkan level_id
+    if ($request->level_id) {
+        $users->where('level_id', $request->level_id);
+    }
+    
     return DataTables::of($users)
         // Menambahkan kolom index/no sort
         ->addIndexColumn()
@@ -53,41 +62,43 @@ class UserController extends Controller
         ->make(true);
     }
 
-        // Menampilkan halaman form tambah user
+    // Menampilkan halaman form tambah user
     public function create()
     {
         $breadcrumb = (object) [
             'title' => 'Tambah User',
-            'list' => ['Home', 'User', 'Tambah']
+            'list'  => ['Home', 'User', 'Tambah']
         ];
 
         $page = (object) [
             'title' => 'Tambah user baru'
         ];
 
-        $level = LevelModel::all(); // ambil data level untuk ditampilkan di form
-        $activeMenu = 'user'; // set menu yang sedang aktif
+        $level = LevelModel::all(); // Ambil data level untuk ditampilkan di form
+        $activeMenu = 'user'; // Set menu yang sedang aktif
 
-        return view('user.create', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
+        return view('user.create', [
+            'breadcrumb' => $breadcrumb,
+            'page'       => $page,
+            'level'      => $level,
+            'activeMenu' => $activeMenu
+        ]);
     }
 
-        // Menyimpan data user baru
+    // Menyimpan data user baru
     public function store(Request $request)
     {
         $request->validate([
             // username harus diisi, berupa string, minimal 3 karakter, dan bernilai unik di tabel m_user kolom username
             'username' => 'required|string|min:3|unique:m_user,username',
-            // nama harus diisi, berupa string, dan maksimal 100 karakter
-            'nama' => 'required|string|max:100',
-            // password harus diisi dan minimal 5 karakter
-            'password' => 'required|min:5',
-            // level_id harus diisi dan berupa angka
-            'level_id' => 'required|integer'
+            'nama' => 'required|string|max:100', // nama harus diisi, berupa string, dan maksimal 100 karakter
+            'password' => 'required|min:5', // password harus diisi dan minimal 5 karakter
+            'level_id' => 'required|integer' // level_id harus diisi dan berupa angka
         ]);
 
         UserModel::create([
             'username' => $request->username,
-            'nama'     => $request->nama,
+            'nama' => $request->nama,
             'password' => bcrypt($request->password), // password dienkripsi sebelum disimpan
             'level_id' => $request->level_id
         ]);
@@ -95,10 +106,14 @@ class UserController extends Controller
         return redirect('/user')->with('success', 'Data user berhasil disimpan');
     }
 
-        // Menampilkan detail user
+    // Menampilkan detail user
     public function show(string $id)
     {
         $user = UserModel::with('level')->find($id);
+
+        if (!$user) {
+            return redirect()->route('user.index')->with('error', 'User tidak ditemukan');
+        }
 
         $breadcrumb = (object) [
             'title' => 'Detail User',
@@ -109,7 +124,7 @@ class UserController extends Controller
             'title' => 'Detail User'
         ];
 
-        $activeMenu = 'user'; // set menu yang sedang aktif
+        $activeMenu = 'user'; // Set menu yang sedang aktif
 
         return view('user.show', [
             'breadcrumb' => $breadcrumb,
@@ -120,30 +135,31 @@ class UserController extends Controller
     }
 
     // Menampilkan halaman form edit user
-public function edit(string $id)
-{
-    $user = UserModel::find($id);
-    $level = LevelModel::all();
+    public function edit(string $id)
+    {
+        $user = UserModel::find($id);
+        $level = LevelModel::all();
 
-    $breadcrumb = (object) [
-        'title' => 'Edit User',
-        'list' => ['Home', 'User', 'Edit']
-    ];
+        $breadcrumb = (object) [
+            'title' => 'Edit User',
+            'list' => ['Home', 'User', 'Edit']
+        ];
 
-    $page = (object) [
-        'title' => 'Edit user'
-    ];
+        $page = (object) [
+            'title' => 'Edit user'
+        ];
 
-    $activeMenu = 'user'; // set menu yang sedang aktif
+        $activeMenu = 'user'; // set menu yang sedang aktif
 
-    return view('user.edit', [
-        'breadcrumb' => $breadcrumb,
-        'page' => $page,
-        'user' => $user,
-        'level' => $level,
-        'activeMenu' => $activeMenu
-    ]);
-}
+        return view('user.edit', [
+            'breadcrumb' => $breadcrumb,
+            'page' => $page,
+            'user' => $user,
+            'level' => $level,
+            'activeMenu' => $activeMenu
+        ]);
+    }
+
 
     // Menyimpan perubahan data user
     public function update(Request $request, string $id)
@@ -167,23 +183,22 @@ public function edit(string $id)
         return redirect('/user')->with('success', 'Data user berhasil diubah');
     }
 
-    // Menghapus data user
-    public function destroy(string $id)
-    {
-        $check = UserModel::find($id);
-        if (!$check) {  // untuk mengecek apakah data user dengan id yang dimaksud ada atau tidak
-            return redirect('/user')->with('error', 'Data user tidak ditemukan');
-        }
-
-        try {
-            UserModel::destroy($id);  // Hapus data level
-
-            return redirect('/user')->with('success', 'Data user berhasil dihapus');
-        } catch (\Illuminate\Database\QueryException $e) {
-
-            // Jika terjadi error ketika menghapus data, redirect kembali ke halaman dengan membawa pesan error
-            return redirect('/user')->with('error', 'Data user gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
+     // Menghapus data user
+     public function destroy(string $id)
+     {
+         $check = UserModel::find($id);
+         if (!$check) {  // untuk mengecek apakah data user dengan id yang dimaksud ada atau tidak
+             return redirect('/user')->with('error', 'Data user tidak ditemukan');
+         }
+ 
+         try {
+             UserModel::destroy($id);  // Hapus data level
+ 
+             return redirect('/user')->with('success', 'Data user berhasil dihapus');
+         } catch (\Illuminate\Database\QueryException $e) {
+ 
+             // Jika terjadi error ketika menghapus data, redirect kembali ke halaman dengan membawa pesan error
+             return redirect('/user')->with('error', 'Data user gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
         }
     }
-
 }
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -10,24 +10,39 @@
     </div>
     <div class="card-body">
         @if (session('success'))
-            <div class="alert alert-success">{{ session('success') }}</div>
+        <div class="alert-success">{{ session('success') }}</div>
         @endif
         @if (session('error'))
-            <div class="alert alert-danger">{{ session('error') }}</div>
+        <div class="alert-danger">{{ session('error') }}</div>
         @endif
-    
+        <div class="row">
+            <div class="col-md-12">
+                <div class="form-group row">
+                    <label class="col-1 control-label col-form-label">Filter:</label>
+                    <div class="col-3">
+                        <select class="form-control" id="level_id" name="level_id" required>
+                            <option value="">- Semua -</option>
+                            @foreach($level as $item)
+                                <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                            @endforeach
+                        </select>
+                        <small class="form-text text-muted">Level Pengguna</small>
+                    </div>
+                </div>
+            </div>
+        </div>
         <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
             <thead>
                 <tr>
                     <th>ID</th>
                     <th>Username</th>
                     <th>Nama</th>
-                    <th>Level Pengguna</th>
-                    <th>Aksi</th>
+                    <th>User Level</th>
+                    <th>Action</th>
                 </tr>
             </thead>
         </table>
-    </div>    
+    </div>
 </div>
 @endsection
 
@@ -43,16 +58,24 @@ $(document).ready(function() {
         ajax: {
             url: "{{ url('user/list') }}",
             type: "POST",
-            dataType: "json"
+            dataType: "json",
+            data: function (d) {
+                d.level_id = $('#level_id').val();
+            }
         },
         columns: [
             { data: "DT_RowIndex", className: "text-center", orderable: false, searchable: false },
             { data: "username", className: "", orderable: true, searchable: true },
-            { data: "name", className: "", orderable: true, searchable: true },
+            { data: "nama", className: "", orderable: true, searchable: true },
             { data: "level.level_nama", className: "", orderable: false, searchable: false },
             { data: "action", className: "", orderable: false, searchable: false }
         ]
     });
+
+    $('#level_id').on('change', function() {
+        dataUser.ajax.reload();
+    });
+    
 });
 </script>
 @endpush
