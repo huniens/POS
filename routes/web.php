0af05d2846699a4d7f86a2cef668a71332e0b027
huniens <huniens06@gmail.com>
2025-03-27 09:59:54 +0700

    JOBSHEET 6 - Practicum 1

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [WelcomeController::class, 'index']);
 
 Route::group(['prefix' => 'user'], function () {
-    Route::get('/', [UserController::class, 'index']);  // menampilkan halaman awal user
-    Route::post('/list', [UserController::class, 'list']);  // menampilkan data user dalam bentuk json untuk datatables
-    Route::get('/create', [UserController::class, 'create']);  // menampilkan halaman form tambah user
-    Route::post('/', [UserController::class, 'store']);  // menyimpan data user baru
-    Route::get('/{id}', [UserController::class, 'show']);  // menampilkan detail user
-    Route::get('/{id}/edit', [UserController::class, 'edit']);  // menampilkan halaman form edit user
-    Route::put('/{id}', [UserController::class, 'update']);  // menyimpan perubahan data user
-    Route::delete('/{id}', [UserController::class, 'destroy']);  // menghapus data user
+    Route::get('/', [UserController::class, 'index']);          // Menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);       // Menampilkan data user dalam bentuk JSON untuk DataTables
+    Route::get('/create', [UserController::class, 'create']);    // Menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']);          // Menyimpan data user baru
+    Route::get('/create_ajax', [UserController::class, 'create_ajax']); // Menampilkan halaman form tambah user AJAX
+    Route::post('/ajax', [UserController::class, 'store_ajax']);        // Menyimpan data user baru AJAX
+    Route::get('/{id}', [UserController::class, 'show']);        // Menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);   // Menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);      // Menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']);  // Menghapus data user
 });
