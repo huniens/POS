784a5ca8342828b6b790e67b49bb42029a624943
huniens <huniens06@gmail.com>
2025-05-08 14:19:42 +0700

    JS 7 - 2

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,44 +2,42 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Monolog\Level;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+
     protected $fillable = [
-        'name',
-        'email',
+        'username',
+        'nama',
         'password',
+        'level_id',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
-        'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function level()
+    {
+        return $this->belongsTo(Level::class, 'level_id');
+    }
+
+    public function hasRole($kode)
+    {
+        return $this->level && $this->level->level_kode === $kode;
+    }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,27 +15,44 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         $data = [
+            // [
+            //     'user_id' => 1,
+            //     'level_id' => 1,
+            //     'username' => 'admin',
+            //     'nama' => 'Administrator',
+            //     'password' => Hash::make('12345'), //class untuk mengenkripsi/hash password
+            // ],
+            // [
+            //     'user_id' => 2,
+            //     'level_id' => 2,
+            //     'username' => 'manager',
+            //     'nama' => 'Manager',
+            //     'password' => Hash::make('12345'),
+            // ],
+            // [
+            //     'user_id' => 3,
+            //     'level_id' => 3,
+            //     'username' => 'staff',
+            //     'nama' => 'Staff/Kasir',
+            //     'password' => Hash::make('12345'),
+            // ],
+
+            //[
+                //'user_id' => 34,
+                //'level_id' => 1,
+                //'username' => 'adminajah',
+                //'nama' => 'Administrator',
+                //'password' => Hash::make('12345678'), //class untuk mengenkripsi/hash password
+            //],
+
             [
-                'user_id' => 1,
-                'level_id' => 1,
-                'username' => 'admin',
-                'nama' => 'Administrator',
-                'password' => Hash::make('12345'), //class untuk mengenkripsi/hash password
+                'user_id' => 35,
+                'level_id' => 3, // 3 = STF
+                'username' => 'staff1',
+                'nama' => 'Staff Kasir',
+                'password' => Hash::make('12345678'),
             ],
-            [
-                'user_id' => 2,
-                'level_id' => 2,
-                'username' => 'manager',
-                'nama' => 'Manager',
-                'password' => Hash::make('12345'),
-            ],
-            [
-                'user_id' => 3,
-                'level_id' => 3,
-                'username' => 'staff',
-                'nama' => 'Staff/Kasir',
-                'password' => Hash::make('12345'),
-            ],
+            
         ];
         DB::table('m_user')->insert($data);
     }
