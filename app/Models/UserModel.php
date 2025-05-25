0901585dc3ce109531e1b493cd1007389e0c2e3d
huniens <huniens06@gmail.com>
2025-05-25 19:25:49 +0700

    Jobsheet 9 - Practicum 1 – Creating a RESTful API Register

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,62 +2,71 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Middleware\Authorize;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Authenticatable
+class UserModel extends  Authenticatable implements JWTSubject
 {
+
+    public function getJWTIdentifier()
+    {
+        return $this->getkey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return[];
+    }
     use HasFactory;
 
-    protected $table = 'm_user';
-    protected $primaryKey = 'user_id';
-    protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
+    protected $table = 'm_user'; // Mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
 
-    protected $hidden = ['password']; // jangan di tampilkan saat select
+    protected $fillable = ['level_id', 'username', 'nama', 'password', 'created_at', 'updated_at', 'image'];
 
-    protected $casts  = ['password' => 'hashed']; // casting password agar otomatis di hash
+    protected $hidden   = ['password'];
 
-    /**
-     * Relasi ke tabel level
-     */
+    protected $casts    = ['password' => 'hashed'];
+
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
-    
     /**
- * Mendapatkan nama role
- */
-public function getRoleName(): string
-{
-    return $this->level ? $this->level->level_nama : 'Tidak ada level';
-}
-
-/**
- * Cek apakah user memiliki role tertentu
- */
-    public function hasRole(string $role): bool
+     * Mendapatkan nama role dari user
+     */
+    public function getRoleName(): string
     {
-        return $this->level->level_kode == $role;
+        return $this->level ? $this->level->level_nama : 'Tidak ada level';
+        //return $this->level->level_nama;
     }
 
-
-        /**
-     * Mendapatkan kode role
+    /**
+     * Cek apakah user memiliki role tertentu
      */
+    public function hasRole($role): bool
+    {
+        return $this->level && $this->level->level_kode === $role;
+    }
     public function getRole()
     {
-        return $this->level->level_kode;
+        return $this->level ? $this->level->level_kode : null;
     }
-
+    // protected function image(): Attribute
+    // {
+    //     return Attribute::make(
+    //         get: fn ($image) => url($image)
+    //     );
+    // }
     public function getProfilePictureUrl()
     {
         return $this->image
             ? asset($this->image) // karena langsung di public/
             : asset('adminlte/dist/img/user2-160x160.jpg');
     }
-
 }
